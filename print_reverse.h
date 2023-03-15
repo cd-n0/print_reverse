18c5818b2bab678aa6a96be6bb33184cc2d6833a
sezarfen <110563468+sezarfen@users.noreply.github.com>
2023-03-15 23:13:53 +0300

    Updating print_reverse with 2 different approach
    Some additions

--- print_reverse.h
+++ print_reverse.h
@@ -1,9 +1,24 @@
 #include <string.h>
 #include <unistd.h>
+
+// APPROACH 1
 int print_reverse(char *str) {
     int len = strlen(str);  // Determine the length of the input string.
     for (int i = len - 1; i >= 0; i--) {  // Loop through the characters of the string in reverse order.
         write(1, &str[i], 1);  // Write each character to standard output.
     }
     return 0;  // Return zero to indicate success.
+}
+
+// APPROACH 2
+int print_reverse2(char *str)
+{
+    if (!*str)
+        return 1;
+    if (print_reverse2(str + 1) == 1)
+    {
+        write(1, str, 1);
+        return 1;
+    }
+    return -1; // that mean's , an something happened unexcepted
 }
