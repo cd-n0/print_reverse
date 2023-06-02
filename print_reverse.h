09d5f98a808fa16219c430a880866b812fc63ab8
CD-N0 <cdn0@soydev.xyz>
2023-06-02 10:40:56 +0300

    Made functions inline to avoid ODR violations

--- print_reverse.h
+++ print_reverse.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 
 // APPROACH 1
-int print_reverse(char *str) {
+inline int print_reverse(char *str) {
     int len = strlen(str);  // Determine the length of the input string.
     for (int i = len - 1; i >= 0; i--) {  // Loop through the characters of the string in reverse order.
         write(1, &str[i], 1);  // Write each character to standard output.
@@ -14,7 +14,7 @@ int print_reverse(char *str) {
 }
 
 // APPROACH 2
-int print_reverse2(char *str)
+inline int print_reverse2(char *str)
 {
     if (!*str)
         return 1;
