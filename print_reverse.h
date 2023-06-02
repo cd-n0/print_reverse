6e49ad20c0d4db7c183c0bbf51c1ae6c268e745b
CD-N0 <cdn0@soydev.xyz>
2023-06-02 10:45:13 +0300

    Refactoring comments for C89 compatibility

--- print_reverse.h
+++ print_reverse.h
@@ -4,16 +4,16 @@
 #include <string.h>
 #include <unistd.h>
 
-// APPROACH 1
+/* APPROACH 1 */
 inline int print_reverse(char *str) {
-    int len = strlen(str);  // Determine the length of the input string.
-    for (int i = len - 1; i >= 0; i--) {  // Loop through the characters of the string in reverse order.
-        write(1, &str[i], 1);  // Write each character to standard output.
+    int len = strlen(str);  /* Determine the length of the input string. */
+    for (int i = len - 1; i >= 0; i--) {  /* Loop through the characters of the string in reverse order. */
+        write(1, &str[i], 1);  /* Write each character to standard output. */
     }
-    return 0;  // Return zero to indicate success.
+    return 0;  /* Return zero to indicate success. */
 }
 
-// APPROACH 2
+/* APPROACH 2 */
 inline int print_reverse2(char *str)
 {
     if (!*str)
@@ -23,7 +23,7 @@ inline int print_reverse2(char *str)
         write(1, str, 1);
         return 1;
     }
-    return -1; // that mean's, a something happened unexcepted
+    return -1; /* that means, something unexcepted happened */
 }
 
-#endif // PRINT_REVERSE_H
+#endif /* PRINT_REVERSE_H */
