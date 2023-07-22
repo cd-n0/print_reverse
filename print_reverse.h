e2d5ac11336d25df9a14323e61ee8bb3518e34d6
CD-N0 <cdn0@soydev.xyz>
2023-07-22 03:05:06 +0300

    Function documentations

--- print_reverse.h
+++ print_reverse.h
@@ -1,8 +1,20 @@
 #ifndef PRINT_REVERSE_H
 #define PRINT_REVERSE_H
 
+/**
+ * Print the given string in reverse order using an iterative approach.
+ *
+ * @param str The input string to be printed in reverse.
+ * @return 0 on success, or -1 if an unexpected error occurs.
+ */
 int print_reverse_iterative(char *str);
 
+/**
+ * Print the given string in reverse order using a recursive approach.
+ *
+ * @param str The input string to be printed in reverse.
+ * @return 1 on success, or -1 if an unexpected error occurs.
+ */
 int print_reverse_recursive(char *str);
 
 #endif /* PRINT_REVERSE_H */
